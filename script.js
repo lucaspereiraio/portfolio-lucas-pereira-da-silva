5dc497f4c94e73508a54453b1891cdc4671024fb
Lucas Pereira da Silva <lucas.dev.pereira@gmail.com>
2024-07-04 12:31:00 -0300

    toggle-theme-started

--- script.js
+++ script.js
@@ -1,56 +1,83 @@
-var pages = {};
+document.addEventListener("DOMContentLoaded", () => {
+  // Pages navigation and routes
+  var pages = {};
 
-async function fetchpages() {
-  const contentDiv = document.getElementById("app");
-  contentDiv.innerHTML = "Loading page...";
+  async function fetchpages() {
+    const contentDiv = document.getElementById("app");
+    contentDiv.innerHTML = "Loading page...";
 
-  await fetch("pages/resume/resume.html")
-    .then((res) => res.text())
-    .then((data) => (pages.resume = data));
-  await fetch("pages/home/home.html")
-    .then((res) => res.text())
-    .then((data) => (pages.home = data));
-  await fetch("pages/projects/projects.html")
-    .then((res) => res.text())
-    .then((data) => (pages.projects = data));
-  await fetch("pages/contact/contact.html")
-    .then((res) => res.text())
-    .then((data) => (pages.contact = data));
-}
+    await fetch("pages/resume/resume.html")
+      .then((res) => res.text())
+      .then((data) => (pages.resume = data));
+    await fetch("pages/home/home.html")
+      .then((res) => res.text())
+      .then((data) => (pages.home = data));
+    await fetch("pages/projects/projects.html")
+      .then((res) => res.text())
+      .then((data) => (pages.projects = data));
+    await fetch("pages/contact/contact.html")
+      .then((res) => res.text())
+      .then((data) => (pages.contact = data));
+  }
 
-fetchpages()
-  .then(() => {
-    function getContent(pageId) {
-      return pages[pageId];
-    }
-
-    function loadContent() {
-      // faz com que a variavel contentDiv seja a <div id="app" />
-      const contentDiv = document.getElementById("app");
-      // pega qual a hashtag (#) que esta no endereco da pagina
-      pageId = location.hash.substring(1);
-      // variavel conteudo da pagina recebe o retorno da funcao pegarConteudo()
-      const pageContent = getContent(pageId);
-      // adiciona na variavePaginal contentDiv (<div id="app" />) o conteudo da pagina
-      contentDiv.innerHTML = pageContent;
-      //verifica se a pagina eh a pagina contact. Se for adiciona o endereco do arquivo html na variavel src
-      var src = pageId === "contact" ? "pages/contact/contact.js" : undefined;
-      // verifica se a variavel src tem algum valor. Se tiver gera a tag <script /> na sessao head da pagina index.html
-      if (src) {
-        var script = document.createElement("script");
-        script.setAttribute("src", src);
-        document.getElementsByTagName("head")[0].appendChild(script);
+  fetchpages()
+    .then(() => {
+      function getContent(pageId) {
+        return pages[pageId];
       }
+
+      function loadContent() {
+        // faz com que a variavel contentDiv seja a <div id="app" />
+        const contentDiv = document.getElementById("app");
+        // pega qual a hashtag (#) que esta no endereco da pagina
+        pageId = location.hash.substring(1);
+        // variavel conteudo da pagina recebe o retorno da funcao pegarConteudo()
+        const pageContent = getContent(pageId);
+        // adiciona na variavePaginal contentDiv (<div id="app" />) o conteudo da pagina
+        contentDiv.innerHTML = pageContent;
+        //verifica se a pagina eh a pagina contact. Se for adiciona o endereco do arquivo html na variavel src
+        var src = pageId === "contact" ? "pages/contact/contact.js" : undefined;
+        // verifica se a variavel src tem algum valor. Se tiver gera a tag <script /> na sessao head da pagina index.html
+        if (src) {
+          var script = document.createElement("script");
+          script.setAttribute("src", src);
+          document.getElementsByTagName("head")[0].appendChild(script);
+        }
+      }
+
+      if (!location.hash) {
+        location.hash = "#home";
+      }
+
+      loadContent();
+
+      window.addEventListener("hashchange", loadContent);
+    })
+    .catch(() => {
+      document.getElementById("app").innerHTML = "Error ao carregar a pagina";
+    });
+
+  // Toggle theme button
+  const themeToggleBtn = document.getElementById("toggle-theme");
+  const themeIcon = document.getElementById("toggle-icon");
+
+  // Definir o tema inicial com base na preferência do usuário
+  if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-theme");
+    themeIcon.classList.remove("sun-icon");
+    themeIcon.classList.add("moon-icon");
+  }
+
+  themeToggleBtn.addEventListener("click", () => {
+    const isDarkTheme = document.body.classList.toggle("dark-theme");
+    if (isDarkTheme) {
+      themeIcon.classList.remove("sun-icon");
+      themeIcon.classList.add("moon-icon");
+      localStorage.setItem("theme", "dark");
+    } else {
+      themeIcon.classList.remove("moon-icon");
+      themeIcon.classList.add("sun-icon");
+      localStorage.setItem("theme", "light");
     }
-
-    if (!location.hash) {
-      location.hash = "#home";
-    }
-
-    loadContent();
-
-    window.addEventListener("hashchange", loadContent);
-  })
-  .catch(() => {
-    document.getElementById("app").innerHTML = "Error ao carregar a pagina";
   });
+});
